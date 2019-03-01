13209a0697d76f4bbfb7b06cfd20c165028eab99
Jean Paul Bolaños Torres <jbolanos@aligntech.com>
2019-03-01 10:30:40 -0600

    Added first eclipse files

--- testAlign/src/testAlign/Test1.java
+++ testAlign/src/testAlign/Test1.java
@@ -16,6 +16,7 @@ public class Test1 {
 		driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input")).sendKeys("berrics");
 		driver.findElement(By.cssSelector("#tsf > div:nth-child(2) > div > div.FPdoLc.VlcLAe > center > input[type=\"submit\"]:nth-child(1)")).click();
 		driver.navigate().back();
+		driver.navigate().forward();
 		driver.quit();
 		
 		
